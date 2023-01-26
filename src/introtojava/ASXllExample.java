d23cd1a22fda4966c55eea61ecb9a6394b339833
Robel <robelmulugeta416@gmail.com>
2023-01-26 16:55:07 -0500

    Pushing my files, Hello World! is in the src file

---
+++ src/introtojava/ASXllExample.java
@@ -0,0 +1,13 @@
+package introtojava;
+
+public class ASXllExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char var = 'A';
+		int num = var;
+		
+		System.out.println("The ASCII number for " + var + " = " + num);
+	}
+
+}
